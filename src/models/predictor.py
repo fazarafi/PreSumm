b51562b83800edd8fc6478873f66b4672244f3f9
Faza Thirafi <faza.thirafi@gmail.com>
2021-09-07 13:25:07 +0900

    print src on debug

--- src/models/predictor.py
+++ src/models/predictor.py
@@ -155,8 +155,10 @@ class Translator(object):
                     pred, gold, src = trans
                     pred_str = pred.replace('[unused0]', '').replace('[unused3]', '').replace('[PAD]', '').replace('[unused1]', '').replace(r' +', ' ').replace(' [unused2] ', '<q>').replace('[unused2]', '').strip()
                     gold_str = gold.strip()
+                    logger.info("[DEBUG FT] Src: " + str(src))
                     logger.info("[DEBUG FT] Pred_str: " + str(pred_str))
                     logger.info("[DEBUG FT] Gold_str: " + str(gold_str))
+                    logger.info("\n\n")
                     if(self.args.recall_eval):
                         _pred_str = ''
                         gap = 1e3
